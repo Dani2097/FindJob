3455e0041e9f87a6dce656a7157b5745daa47f50
Dani2097 <danilo.sprovieri9@gmail.com>
2019-05-02 15:35:18 +0200

    chissa se va bene

--- DB/DBUtenti.php
+++ DB/DBUtenti.php
@@ -31,7 +31,7 @@ class DBUtenti
             "edizione",
             "casa_editrice",
             "cod_stud",
-            "autore",
+            "Curriculum",
             "cod_materia"
         ],
         "cdl" => [
